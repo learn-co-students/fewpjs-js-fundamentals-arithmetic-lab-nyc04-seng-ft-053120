233cef74b585d77e98feac8d5a11cc5c7a1b6d8a
AlveeM <alvee.akand@outlook.com>
2020-08-08 11:58:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.round(Math.random()) + 1
+
+let mod = 39 % 7
+
+let max = Math.max(5, 10, 15, 20)
