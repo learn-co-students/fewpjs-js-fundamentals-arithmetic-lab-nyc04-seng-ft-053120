4179aec40dbf3c73ae1dc58b6d7a3bf70abf9093
vuonga1103 <vuonga1103@yahoo.com>
2020-08-11 10:59:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Create a variable called multiply that will multiply the variables num1 and num2 whose results will equal "62".
-const num1 = 31;
-const num2 = 2;
+const num1 = 31, num2 = 2;
 const multiply = num1 * num2;
 
 // Create a variable called random that will generate a random integer greater than 0.
